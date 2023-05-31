e786e0e68f17184ab11611fcb20df9469608baf5
Matheus Sodré <matheus.motasodre.5@gmail.com>
2023-05-31 14:58:02 -0300

    Atualização do código-fonte

---
+++ js/connection.js
@@ -0,0 +1,409 @@
+const url = 'https://2bf9-2804-14c-5980-4053-5560-dad3-3b3e-5d92.ngrok-free.app';
+
+function loadChatBot() {
+    new BlipChat()
+        .withAppKey('ZmVpdG9wb3Jub3M6ZGNmMzY2OTAtZWM0YS00MGYyLWJlZTctOTJjZmRiODI3OWIz')
+        .withButton({ "color": "#ff8800", "icon": "" })
+        .withCustomCommonUrl('https://matheus-sodre-yaanj.chat.blip.ai/')
+        .build();
+}
+
+async function verifyConnection() {
+
+    let userLogged = JSON.parse(localStorage.getItem('userLogged'));
+
+    if (userLogged) {
+        let carrinho = JSON.parse(localStorage.getItem('carrinho'));
+
+        if (carrinho.length)
+            updateCartValue(carrinho.length);
+
+        if (getEndPoint() != 'login.html') {
+            if (userLogged.profile == 'seller' && document.querySelector('#btnLogin')) {
+                document.querySelector('#btnLogin').href = '/pages/menuUser/userComum.html';
+                let icons = document.querySelectorAll('.navbar__item');
+
+                icons.forEach((element, index) => {
+                    if (index != 3)
+                        element.style.display = 'inline';
+                });
+
+                let firstName = (userLogged.nome.split(' '))[0];
+                document.querySelector('#logintransform').innerText = firstName;
+            }
+
+            if (userLogged.profile == 'user' && document.querySelector('#btnLogin')) {
+                document.querySelector('#btnLogin').href = '/pages/menuUser/userComum.html';
+                let icons = document.querySelectorAll('.navbar__item');
+
+                icons.forEach((element, index) => {
+                    if (index == 0 || index == 4)
+                        element.style.display = 'inline';
+                });
+
+                let firstName = (userLogged.nome.split(' '))[0];
+                document.querySelector('#logintransform').innerText = firstName;
+            }
+        } else {
+            window.location.href = '/pages/main.html';
+        }
+    }
+}
+
+async function connectionOn() {
+
+    const data = {
+        email: document.querySelector('#email-login').value,
+        senha: document.querySelector('#password-login').value
+    };
+
+
+    const response = await fetch(url + '/set/connection', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+
+    if (response.status == 200) {
+        let person = await response.json();
+
+        localStorage.setItem('userLogged', JSON.stringify(person));
+        localStorage.setItem('carrinho', '[]');
+
+        if (localStorage.getItem('productToCar')) {
+            let href = localStorage.getItem('productToCar');
+            localStorage.removeItem('productToCar');
+            window.location.href = href;
+        } else
+            window.location.href = '/pages/main.html'
+
+    } else {
+        alert('Usuário não encontrado');
+    }
+
+}
+
+async function connectionOff() {
+    //get item session storage
+    let userLogged = JSON.parse(localStorage.getItem('userLogged'));
+
+    await fetch(url + '/set/connection/loggout', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userLogged),
+    })
+
+    localStorage.clear();
+    window.location.href = '/pages/main.html';
+}
+
+async function loadProfile() {
+
+    let userLogged = JSON.parse(localStorage.getItem('userLogged'));
+
+    if (userLogged) {
+        document.querySelector('#profileName').innerText = userLogged.nome;
+        document.querySelector('#profileEmail').innerText = userLogged.email;
+    } else {
+        window.location.href = '/pages/main.html';
+    }
+}
+
+async function openProduct(categoria) {
+    const body = {
+        allProducts: true
+    }
+
+    const response = await fetch(url + '/get/products/all', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    })
+
+    const products = (await response.json())[categoria];
+    console.log(products)
+
+    if (products.length) {
+        document.querySelector('#semproduto').style.display = 'none';
+        document.querySelector('#categoriaCabe').style.display = 'contents';
+        document.querySelector('#categoriaPage').innerHTML += getBody(products, categoria);
+    } else
+        document.querySelector('#semproduto').style.display = 'flex';
+
+}
+
+function getBody(products, categoria) {
+    let body = '';
+    console.log("---> " + products)
+    products.forEach(element => {
+        body += `<tr>
+        <td>
+            <div class="product">
+                    <img src="/assets/img/cosmeticosDaIlha.png" alt="">
+                <div class="info">
+                    <div class="name">${element.nomeProduto}</div>
+                    <div class="categoria">${element.categoria}</div>
+                </div>
+            </div>
+        </td>
+        <td id="price">R$ ${element.precoProduto},00</td>
+        <td><button class="comprarProduto" onclick="openProductPage(${element.sellerId}, ${element.id},'${categoria}','${element.categoria}','${element.nomeProduto}', ${element.precoProduto})">Ver Detalhes</button></td>
+    </tr>`
+    });
+
+    return `<tbody>${body}</tbody>`;
+}
+
+async function openProductPage(sellerId, id, categoria, fullCategoria, nome, price) {
+
+    const choosedProduct = {
+        sellerId,
+        id,
+        categoria,
+        fullCategoria,
+        nome,
+        price
+    }
+
+    localStorage.setItem('product', JSON.stringify(choosedProduct));
+    window.location.href = '/pages/pagina_produtos/pagina_produto.html';
+}
+
+function plusCounter(id = 'empty') {
+
+    if (id != 'empty') {
+        let currentValue = parseInt(document.querySelectorAll(".productCounter")[id].innerText);
+        currentValue++;
+
+        let carrinho = JSON.parse(updateInsideCart(id, currentValue));
+
+        document.querySelectorAll('.cartTotal')[id].innerText = carrinho[id].qtd * carrinho[id].product.price;
+        document.querySelectorAll(".productCounter")[id].innerText = currentValue;
+        updateTotalCart();
+    } else {
+        let currentValue = parseInt(document.querySelector("#productCounter").innerText);
+
+        currentValue++;
+        document.querySelector("#productCounter").innerText = currentValue;
+    }
+}
+
+function minusCounter(id = 'empty') {
+
+    if (id != 'empty') {
+        let currentValue = parseInt(document.querySelectorAll(".productCounter")[id].innerText);
+
+        if (currentValue > 1)
+            currentValue--;
+
+        let carrinho = JSON.parse(updateInsideCart(id, currentValue));
+
+        document.querySelectorAll('.cartTotal')[id].innerText = carrinho[id].qtd * carrinho[id].product.price;
+        document.querySelectorAll(".productCounter")[id].innerText = currentValue;
+        updateTotalCart();
+    } else {
+        let currentValue = parseInt(document.querySelector("#productCounter").innerText);
+
+        if (currentValue > 0)
+            currentValue--;
+
+        document.querySelector("#productCounter").innerText = currentValue;
+    }
+}
+
+async function loadProduct() {
+
+    let infoProduct = JSON.parse(localStorage.getItem('product'));
+
+    const body = {
+        allProducts: true
+    }
+
+    const response = await fetch(url + '/get/products/all', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    })
+
+    const products = await response.json();
+
+    let choosedProduct = products[infoProduct.categoria].find(element =>
+        element.sellerId == infoProduct.sellerId && element.id == infoProduct.id);
+
+    document.querySelector('#productCategory').innerText = choosedProduct.categoria;
+    document.querySelector('#productName').innerText = choosedProduct.nomeProduto;
+    document.querySelector('#productDescription').innerText = choosedProduct.descricao;
+    document.querySelector('#price').innerText = `R$${choosedProduct.precoProduto},00`;
+    document.querySelector('#promoPrice').innerText = `R$${choosedProduct.precoProduto * 0.8},00`;
+}
+
+function getEndPoint() {
+    let endPoint = window.location.href;
+    endPoint = endPoint.split('/');
+    endPoint = endPoint[endPoint.length - 1];
+
+    return endPoint;
+}
+
+
+function sendProduct() {
+    let userLogged = JSON.parse(localStorage.getItem('userLogged'));
+    let infoProduct = JSON.parse(localStorage.getItem('product'));
+    let qtd = document.querySelector('#productCounter').innerText;
+
+    if (qtd == '0')
+        return alert('Informe uma quantidade!');
+
+    if (!userLogged) {
+        localStorage.setItem('productToCar', `${window.location.href}`);
+        window.location.href = '/pages/login/login.html';
+    } else {
+        console.log(infoProduct, qtd)
+        addToCart(infoProduct, qtd);
+        console.log(JSON.parse(localStorage.getItem('carrinho')))
+        document.querySelector("#productCounter").innerText = 0;
+    }
+}
+
+function addToCart(infoProduct, quantidade) {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho'));
+    let inCart = carrinho.some(element =>
+        element.product.sellerId == infoProduct.sellerId &&
+        element.product.id == infoProduct.id &&
+        element.product.categoria == infoProduct.categoria
+    )
+    quantidade = parseInt(quantidade);
+
+    if (inCart) {
+        carrinho.forEach((element, index) => {
+            if (element.product.sellerId == infoProduct.sellerId && element.product.id == infoProduct.id) {
+                carrinho[index].qtd = carrinho[index].qtd + quantidade;
+            }
+        })
+    } else
+        carrinho.push({ product: infoProduct, qtd: quantidade });
+
+    let carrinhoSize = carrinho.length;
+
+    updateCartValue(carrinhoSize);
+    carrinho = JSON.stringify(carrinho);
+    localStorage.setItem('carrinho', carrinho);
+}
+
+function updateCartValue(carrinhoSize) {
+    document.querySelector('#cartValue').innerText = carrinhoSize;
+}
+
+function updateInsideCart(id, quantidade, remove = false) {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho'));
+
+    if (remove) {
+        carrinho[id] = null;
+        carrinho = carrinho.filter(element => element !== null);
+    } else
+        carrinho[id].qtd = quantidade;
+
+    carrinho = JSON.stringify(carrinho);
+    localStorage.setItem('carrinho', carrinho);
+
+    return carrinho;
+}
+
+function loadCart() {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho'));
+
+    if (carrinho.length) {
+        document.querySelector('#semproduto').style.display = 'none';
+        document.querySelector('#cartHead').style.display = 'contents';
+        document.querySelector('#cartTable').innerHTML += getCartBody(carrinho);
+        document.querySelector('#asideTotal').style.display = 'inline';
+        updateTotalCart();
+    } else {
+        document.querySelector('#semproduto').style.display = 'flex';
+        document.querySelector('#cartTable').style.display = 'none';
+        document.querySelector('#asideTotal').style.display = 'none';
+    }
+}
+
+function getCartBody(carrinho) {
+    let body = '';
+
+    carrinho.forEach((element, index) => {
+        body += `<tr class="cartTableRow">
+                        <td>
+                            <div style="cursor: pointer;" class="product" onclick="openProductPage('${element.product.sellerId}', '${element.product.id}', '${element.product.categoria}', '${element.product.fullCategoria}', '${element.product.nome}', '${element.product.price}')">
+                                <img src="/assets/img/cosmeticosDaIlha.png" alt="">
+                                <div class="info">
+                                    <div class="name" id="cartName">${element.product.nome}</div>
+                                    <div class="categoria" id="cartCategory">${element.product.fullCategoria}</div>
+                                </div>
+                            </div>
+                            </td>
+                            <td>R$ <span id="cartPrice">${element.product.price}</span>,00</td>
+                        <td>
+                            <div class="qty">
+                                <button class="counterProduct" onclick="minusCounter(${index})"><i class="bx bx-minus" ></i></button>
+                                <span class="productCounter">${element.qtd}</span>
+                                <button class="counterProduct" onclick="plusCounter(${index})"><i class="bx bx-plus" ></i></button>
+                            </div>
+                        </td>
+                        <td>R$ <span class="cartTotal">${element.product.price * element.qtd}</span>,00</td>
+                        <td><button class="remover" onclick="removeProductCart(${index})"><i class="bx bx-x"></i></button></td>
+                    </tr>`
+    });
+
+
+    return `<tbody>${body}</tbody>`
+}
+
+function getTotalCart() {
+    let elements = document.querySelectorAll('.cartTotal');
+    let totalPrice = 0;
+
+    elements.forEach(element => {
+        totalPrice += parseInt(element.innerText);
+    });
+
+    return totalPrice;
+}
+
+function updateTotalCart() {
+    document.querySelector('#cartSubtotalPrice').innerText = getTotalCart();
+    document.querySelector('#cartTotalPrice').innerText = getTotalCart();
+}
+
+function removeProductCart(id) {
+    document.querySelectorAll('.cartTableRow')[id].remove();
+
+    let size = JSON.parse(updateInsideCart(id, null, true)).length;
+    updateCartValue(size);
+    updateTotalCart();
+
+    let elements = document.querySelectorAll('.remover');
+    let buttons = document.querySelectorAll('.counterProduct');
+
+    if (!elements.length) {
+        loadCart();
+
+        return;
+    }
+
+    buttons.forEach((button, index) => {
+        if (index % 2 == 0)
+            button.setAttribute('onclick', `minusCounter(${index / 2})`);
+        else
+            button.setAttribute('onclick', `plusCounter(${(index - 1) / 2})`);
+    })
+
+    elements.forEach((element, index) => {
+        element.setAttribute("onclick", `removeProductCart(${index})`);
+    });
+}
---
+++ js/request.js
@@ -0,0 +1,160 @@
+const url = 'https://2bf9-2804-14c-5980-4053-5560-dad3-3b3e-5d92.ngrok-free.app'
+
+async function cadastrarProdutos() {
+
+    let userLogged = JSON.parse(localStorage.getItem('userLogged'));
+
+    const body = {
+        //link: (document.querySelector('#produto-imagem-preview').src,
+        nomeProduto: document.querySelector("#produto-nome").value.trim()
+            ? document.querySelector("#produto-nome").value
+            : null,
+        categoria: document.querySelector("#produto-categoria").value.trim()
+            ? document.querySelector("#produto-categoria").value
+            : null,
+        precoProduto: document.querySelector("#produto-preco").value.trim()
+            ? document.querySelector("#produto-preco").value
+            : null,
+        descricao: document.querySelector("#produto-desc").value.trim()
+            ? document.querySelector("#produto-desc").value
+            : null,
+        qtd: document.querySelector("#produto-quantidade").value.trim()
+            ? document.querySelector("#produto-quantidade").value
+            : null
+    };
+
+    if (verifyEmptyField(body)) {
+        alert('Preencha todos os campos');
+    } else {
+        document.querySelector("#signButton").disabled = true;
+        await fetch(url + '/set/seller/product', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(
+                {
+                    seller: userLogged,
+                    product: body
+                }
+            ),
+        });
+
+        setTimeout(() => {
+            window.location.reload(true);
+        }, 600);
+    }
+}
+
+async function cadastrarVendedor() {
+
+    const infoVendedor = {
+        nome: document.querySelector("#nome-completo").value.trim()
+            ? document.querySelector("#nome-completo").value
+            : null,
+        email: document.querySelector("#email").value.trim()
+            ? document.querySelector("#email").value
+            : null,
+        nickname: document.querySelector("#produto-artesanato").value.trim()
+            ? document.querySelector("#produto-artesanato").value
+            : null,
+        telefone: document.querySelector("#telefone").value.trim()
+            ? document.querySelector("#telefone").value
+            : null,
+        senha: document.querySelector("#password").value.trim()
+            ? document.querySelector("#password").value
+            : null
+    };
+
+    if (verifyEmptyField(infoVendedor)) {
+        alert('Preencha todos os campos')
+    } else {
+        document.querySelector("#signSeller").disabled = true;
+        let email = document.getElementsByClassName('email')[0].value;
+        let confirmEmail = document.getElementsByClassName('email')[1].value;
+        let senha = document.getElementsByClassName('senha')[0].value;
+        let confirmSenha = document.getElementsByClassName('senha')[1].value;
+
+        if (email != confirmEmail) {
+            alert("Os email's devem ser iguais!")
+        } else if (senha != confirmSenha) {
+            alert("As senhas devem ser iguais!")
+        } else {
+            const response = await fetch(url + '/set/seller', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(infoVendedor),
+            });
+
+
+            setTimeout(() => {
+                window.location.href = '/pages/login/login.html';
+            }, 600);
+        }
+    }
+}
+
+async function cadastrarUser() {
+
+    const infoUser = {
+        nome: document.querySelector("#nome-completo").value.trim()
+            ? document.querySelector("#nome-completo").value
+            : null,
+        email: document.querySelector("#email").value.trim()
+            ? document.querySelector("#email").value
+            : null,
+        telefone: document.querySelector("#telefone").value.trim()
+            ? document.querySelector("#telefone").value
+            : null,
+        senha: document.querySelector("#password").value.trim()
+            ? document.querySelector("#password").value
+            : null
+    };
+
+    if (verifyEmptyField(infoUser)) {
+        alert('Preencha todos os campos')
+    } else {
+        document.querySelector("#signUser").disabled = true;
+        let email = document.getElementsByClassName('email')[0].value;
+        let confirmEmail = document.getElementsByClassName('email')[1].value;
+        let senha = document.getElementsByClassName('senha')[0].value;
+        let confirmSenha = document.getElementsByClassName('senha')[1].value;
+
+        if (email != confirmEmail) {
+            alert("Os email's devem ser iguais!")
+        } else if (senha != confirmSenha) {
+            alert("As senhas devem ser iguais!")
+        } else {
+            const response = await fetch(url + '/set/user', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(infoUser),
+            });
+
+            setTimeout(() => {
+                window.location.href = '/pages/login/login.html';
+            }, 600);
+        }
+    }
+}
+
+async function getProduct() {
+    const response = await fetch(url + '/set/seller/product/1');
+
+    const text = await response.json();
+    console.log(text)
+}
+
+function verifyEmptyField(obj) {
+    for (let info in obj) {
+        if (!(obj[info])) {
+            return true;
+        }
+    }
+
+    return false;
+}
